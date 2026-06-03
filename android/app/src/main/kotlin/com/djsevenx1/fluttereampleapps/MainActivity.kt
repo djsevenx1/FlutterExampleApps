ee03846e801b6e70d83507ad79090fd684d8d026
DJSeven <djsevenx1@example.com>
2026-06-03 21:50:05 +0800

    feat: Initial Flutter project with working GitHub Actions

---
+++ android/app/src/main/kotlin/com/djsevenx1/fluttereampleapps/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.djsevenx1.fluttereampleapps
+import io.flutter.embedding.android.FlutterActivity
+class MainActivity: FlutterActivity() {}
---
+++ lib/main.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MaterialApp(home: Scaffold(appBar: AppBar(title: Text('FlutterExampleApps')), body: Center(child: Text('Hello from DJSeven!')))));
